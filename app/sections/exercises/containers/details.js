baea7dd0db1ee32b2d593c50a90eb5431b07a4eb
nickve28 <nickvaneyk@gmail.com>
2017-07-08 16:29:54 +0200

    Finalize new exercise detail container

--- app/sections/exercises/containers/details.js
+++ app/sections/exercises/containers/details.js
@@ -1,86 +1,104 @@
-// import React from 'react'
-// import { connect } from 'react-redux'
-// import { Link } from 'react-router'
-// import ChipInput from 'material-ui-chip-input'
-// import { TextField } from 'material-ui'
-// import { withState, withHandlers, compose } from 'recompose'
-// import { without } from 'lodash'
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import ChipInput from 'material-ui-chip-input'
+import { TextField } from 'material-ui'
+import { withState, withHandlers, compose } from 'recompose'
 
-// import { fetchExercise, updateExercise } from '../actions/exercise'
+import { fetchExercise, updateExercise } from '../actions/exercise'
 
-// import Title from '../../../components/title'
+import Title from '../../../components/title'
 
-// import MaterialLabel from '../../../components/material/form/label'
+import MaterialLabel from '../../../components/material/form/label'
 
-// const ExerciseDetails = props => {
-//   const {
-//     exercise,
-//     exerciseUpdate,
-//     fetch,
-//     update,
-//     updateDescription,
-//     addCategory,
-//     removeCategory
-//   } = props
+const ExerciseDetails = props => {
+  const {
+    exercise,
+    exerciseUpdate,
+    fetch,
+    update,
+    updateDescription,
+    addCategory,
+    removeCategory
+  } = props
 
-//   if (!exercise) {
-//     fetch()
-//     return <div>Loading...</div>
-//   }
+  if (!exercise) {
+    fetch()
+    return <div>Loading...</div>
+  }
 
-//   return <div>
-//     <Title title={exercise.name}>
-//       <Link className="pull-right" to="/">Go back</Link>
-//     </Title>
+  return <div>
+    <Title title={exercise.name}>
+      <Link className="pull-right" to="/">Go back</Link>
+    </Title>
 
-//     <div>
-//       <h4>Summary</h4>
-//         <MaterialLabel name="Type" />
-//         <div>{exercise.type}</div>
+    <div>
+      <h4>Summary</h4>
+        <MaterialLabel name="Type" />
+        <div>{exercise.type}</div>
 
-//         <MaterialLabel name="Metric" />
-//         <div>{exercise.metric}</div>
+        <MaterialLabel name="Metric" />
+        <div>{exercise.metric}</div>
 
-//         <MaterialLabel name="Description" />
-//         <TextField
-//           name="description"
-//           value={exerciseUpdate.description || exercise.description}
-//           onChange={updateDescription}
-//           onBlur={() => update(exerciseUpdate)}
-//         />
+        <MaterialLabel name="Description" />
+        <TextField
+          name="description"
+          value={exerciseUpdate.description || exercise.description}
+          onChange={updateDescription}
+          onBlur={() => update(exerciseUpdate)}
+        />
 
-//         <MaterialLabel name="Categories" />
-//         <ChipInput
-//           dataSource={[]}
-//           onRequestAdd={addCategory}
-//           onRequestDelete={removeCategory}
-//           value={exerciseUpdate.categories || exercise.categories}
-//         />
-//     </div>
-//   </div>
-// }
+        <MaterialLabel name="Categories" />
+        <ChipInput
+          dataSource={[]}
+          onRequestAdd={category => {
+            addCategory(category, update) //todo think of a better solution
+          }}
+          onRequestDelete={category => {
+            removeCategory(category, update)
+          }}
+          onChange={() => update(exerciseUpdate)}
+          value={exerciseUpdate.categories || exercise.categories}
+        />
+    </div>
+  </div>
+}
 
-// const mapStateToProps = (state, props) => ({
-//   exercise: state.exercises.data.entities[props.params.id]
-// })
+const mapStateToProps = (state, props) => ({
+  exercise: state.exercises.data.entities[props.params.id]
+})
 
-// const mapDispatchToProps = (dispatch, { params }) => ({
-//   fetch: () => dispatch(fetchExercise(params.id)),
-//   update: exercise => dispatch(updateExercise(exercise))
-// })
+const mapDispatchToProps = (dispatch, { params }) => ({
+  fetch: () => dispatch(fetchExercise(params.id)),
+  update: exercise => dispatch(updateExercise(exercise))
+})
 
-// export default compose(
-//   connect(mapStateToProps, mapDispatchToProps),
-//   withState('exerciseUpdate', 'setExercise', {}),
-//   withHandlers({
-//     updateDescription: props => event => {
-//       props.setExercise({ ...props.exercise, description: event.target.value})
-//     },
-//     addCategory: props => category => props.setExercise(
-//       { ...props.exercise, categories: [...props.exercise.categories, category]}
-//     ),
-//     removeCategory: props => category => props.setExercise(
-//       { ...props.exercise, categories: without(props.exercise.categories, category)}
-//     )
-//   })
-// )(ExerciseDetails)
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  //set exercise (if loaded) as default state
+  withState('exerciseUpdate', 'setExercise', ({ exercise }) => exercise ? exercise: {}),
+  withHandlers({
+    updateDescription: props => event => {
+      props.setExercise({ ...props.exercise, description: event.target.value})
+    },
+    addCategory: props => (category, update) => {
+      const { exercise, setExercise } = props
+      const newCategories = [...exercise.categories, category]
+
+      setExercise({ ...exercise, categories: newCategories})
+      update({ ...exercise, categories: newCategories })
+    },
+    removeCategory: props => (category, update) => {
+      const { exercise, setExercise } = props
+      const { categories } = exercise
+      const categoryIndex = categories.indexOf(category)
+      let newCategories = categories
+      if (categoryIndex > -1) {
+        newCategories.splice(categoryIndex, 1)
+      }
+
+      setExercise({ ...exercise, categories: newCategories})
+      update({ ...exercise, categories: newCategories })
+    }
+  })
+)(ExerciseDetails)
