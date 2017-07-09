7d80165447332ff778003ada1f1a48c003edba18
nickve28 <nickvaneyk@gmail.com>
2017-07-09 13:17:17 +0200

    Added basic redux form login form (Redo)

--- app/sections/authentication/containers/login.js
+++ app/sections/authentication/containers/login.js
@@ -1,14 +1,62 @@
 import React from 'react'
+import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
 
-import { loginUser } from '../actions/authentication'
-import LoginForm from '../components/login_form'
+import Title from 'app/components/title'
+import MaterialField from 'app/components/material/form/field'
+import MaterialLoginButton from 'app/components/material/form/login_button'
 
-const Login = ({ loginUser, loginState }) =>
-  <LoginForm login={loginUser} loginState={loginState} />
+import { required } from 'app/helpers/validations'
+
+import { loginUser } from '../actions/authentication'
+
+let Login = props => {
+  const {
+    handleSubmit,
+    pristine,
+    submitting,
+    loginUser,
+    loginState
+  } = props
+  return (
+    <div>
+      <Title title="Log in" />
+      <form
+        style={{ marginTop: '30px' }}
+        className="login-form"
+        onSubmit={handleSubmit(({username, password}) => loginUser(username, password))}
+      >
+
+      <Field
+        type="text"
+        name="username"
+        label="username"
+        component={MaterialField}
+        validate={required}
+      />
+
+      <Field
+        type="password"
+        name="password"
+        label="password"
+        component={MaterialField}
+        validate={required}
+      />
+
+      <MaterialLoginButton
+        disabled={pristine || submitting || loginState === 'logging_in'}
+        label="Log in"
+      />
+      </form>
+    </div>
+  )
+}
 
 const mapStateToProps = state => ({
   loginState: state.authentication.loginState
 })
 
+Login = reduxForm({
+  form: 'login'
+})(Login)
 export default connect(mapStateToProps, { loginUser })(Login)
